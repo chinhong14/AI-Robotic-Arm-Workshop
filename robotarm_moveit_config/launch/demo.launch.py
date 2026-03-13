89e79ecff5bb10774875d1650b1377fa2dd31a8f
chinhong14 <chinhonglee14@gmail.com>
2026-03-13 17:44:54 +0800

    Updated robotarm_moveit_config

--- robotarm_moveit_config/launch/demo.launch.py
+++ robotarm_moveit_config/launch/demo.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("cocoabot", package_name="robotarm_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("robotarm1", package_name="robotarm_moveit_config").to_moveit_configs()
     return generate_demo_launch(moveit_config)
